cb4b4dbcf35a24587bb98d6895d3241492d6423a
Flyour <wangritian13@gmail.com>
2019-03-08 18:02:51 +0800

    first commit

---
+++ DynamoRIO-Windows-7.1.0-1/docs/html/files_dup.js
@@ -0,0 +1,6 @@
+var files_dup =
+[
+    [ "drmemtrace", "dir_b5d1fe23616d13333ee18b4d9db07e4d.html", "dir_b5d1fe23616d13333ee18b4d9db07e4d" ],
+    [ "dynamorio_package", "dir_0192d84683cf303c47051373dc7e238e.html", "dir_0192d84683cf303c47051373dc7e238e" ],
+    [ "include", "dir_b2f109a271667942dec1cf6807a2f9aa.html", "dir_b2f109a271667942dec1cf6807a2f9aa" ]
+];
---
+++ DynamoRIO-Windows-7.1.0-1/docs/html/navtreedata.js
@@ -0,0 +1,95 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "DynamoRIO API", "index.html", [
+    [ "The DynamoRIO API", "index.html", "index" ],
+    [ "DynamoRIO Extensions", "page_ext.html", "page_ext" ],
+    [ "DynamoRIO-Based Tools", "page_tool.html", "page_tool" ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "DynamoRIO Extension Details", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ],
+    [ "DynamoRIO Home Page", "^http://www.dynamorio.org", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"API_BT.html",
+"dr__config_8h.html#a7d9d2d496f3ef8bdbfbb10c0567673f9",
+"dr__ir__instr_8h.html#a3722cd7a5b3728b5edeefd4b07bd456c",
+"dr__ir__instrlist_8h.html#a2bad28eed64877354dcf984ff672c7d9",
+"dr__ir__macros__arm_8h.html#a00a664d659534e37d37556906b3d4128",
+"dr__ir__macros__arm_8h.html#a2fa45348c39e01358534d26ecc3faaba",
+"dr__ir__macros__arm_8h.html#a67ca8c987676aec2294ed4082d755e1e",
+"dr__ir__macros__arm_8h.html#a98ee3185ba71cae201fab689196e4d4f",
+"dr__ir__macros__arm_8h.html#aca61212b842db3f347df7b024d169b24",
+"dr__ir__macros__arm_8h.html#afc247b4c54fb4c1e3c8642c43a2129da",
+"dr__ir__macros__x86_8h.html#a37aa20d7c67db7fb558c66f67b15b2e9",
+"dr__ir__macros__x86_8h.html#a6cfc8e3341c83d3833b731e90fa2a3c4",
+"dr__ir__macros__x86_8h.html#aa4a3ff51c758a3aa3e205ae96a928120",
+"dr__ir__macros__x86_8h.html#adf84c0572fff7a72ca57b9fdee23c4d6",
+"dr__ir__opcodes__arm_8h.html#a385c44f6fb256e5716a2302a5b940388a1a7893f6cbce8194e3c1dc0ed0f77755",
+"dr__ir__opcodes__arm_8h.html#a385c44f6fb256e5716a2302a5b940388a62d4edc17922f164d384d31b9a043fe7",
+"dr__ir__opcodes__arm_8h.html#a385c44f6fb256e5716a2302a5b940388aa963a65d062b3c940b6d1f2e10292c86",
+"dr__ir__opcodes__arm_8h.html#a385c44f6fb256e5716a2302a5b940388ae847f4e111ff680cdc28f6d4588bb265",
+"dr__ir__opcodes__x86_8h.html#abc5c98fcc1211af2b80116dd6e0a035da1849ea4bd728f3521855902d4156548c",
+"dr__ir__opcodes__x86_8h.html#abc5c98fcc1211af2b80116dd6e0a035da4da610d5768e0744ec9013411fbcb297",
+"dr__ir__opcodes__x86_8h.html#abc5c98fcc1211af2b80116dd6e0a035da84f135d92467f16faa641d7f9480e0ca",
+"dr__ir__opcodes__x86_8h.html#abc5c98fcc1211af2b80116dd6e0a035dac10790aba3a24b0bddd859cd6e9046d7",
+"dr__ir__opcodes__x86_8h.html#abe08c1768c18bc19fcfe8ecf35b660ef",
+"dr__ir__opnd_8h.html#ac0ce8f7566d47fde61f419bcf9ac8b6f",
+"dr__ir__utils_8h.html#a4c1e2fa2049719870ad0c1007b00d4e3a4371407d86fde325673f7d3967037708",
+"dr__proc_8h.html#af1e2126b25bfe95d3803edf2418ff7fe",
+"dr__tools_8h.html#aba205966cb16d0478ecf159cdfeaee8b",
+"globals_func.html",
+"group__drmgr.html#ga4db3e75ae461dc2596d24d0768ab2716",
+"group__drsyms.html#ga75abd525d4d1710196d18d7c13b4c061",
+"group__drx.html#ga8f9c7628a63632568aaeb62beeb3a89a",
+"struct__dr__module__import__t.html#a1f63ee22ef2f15e47b67c835718b6e42",
+"trace__entry_8h.html#a00405f99b069de9812b34db3a192b19daa97f8d1bc58f4fb86f749adf3c4ccd42"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
